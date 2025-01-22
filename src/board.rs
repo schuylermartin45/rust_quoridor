a5f9dc0515186ada21c4eb87040578b2d33629d2
Schuyler Martin <schuylermartin45@gmail.com>
2025-01-21 18:09:31 -0700

    Adjusts some typing and plans out how walls will be handled

--- src/board.rs
+++ src/board.rs
@@ -3,7 +3,7 @@
 //! Description:    Game board representation
 //!
 
-use crate::player::Color;
+use crate::player::{Color, Direction, Player};
 use std::collections::{HashMap, HashSet};
 use std::fmt;
 
@@ -12,20 +12,13 @@ const BOARD_SIZE: usize = 5;
 
 /// A coordinate position in the game board (row,column)
 #[derive(Hash, PartialEq, Eq)]
-struct Point {
-    row: usize,
-    col: usize,
-}
+struct Point(usize, usize);
 
-impl Point {
-    /// Convenience constructor for the Point type
-    pub fn new(r: usize, c: usize) -> Self {
-        let p = Point { row: r, col: c };
-        p
-    }
-}
-
-// TODO Walls defined as 2 upper left points?
+/// A wall is just two pairs of coordinates. Uses the same row/column coordinate
+/// system as the rest of the board (row/column refers to the upper and left
+/// side of a board position)
+#[derive(Hash, PartialEq, Eq)]
+struct Wall(Point, Point);
 
 /// Represents a single space on the board
 struct BoardNode {
@@ -44,16 +37,16 @@ impl BoardNode {
 
         let upper_bound = BOARD_SIZE - 1;
         if row < upper_bound && col < upper_bound {
-            bn.neighbors.insert(Point::new(row + 1, col + 1));
+            bn.neighbors.insert(Point(row + 1, col + 1));
         }
         if row < upper_bound && col > 0 {
-            bn.neighbors.insert(Point::new(row + 1, col - 1));
+            bn.neighbors.insert(Point(row + 1, col - 1));
         }
         if row > 0 && col < upper_bound {
-            bn.neighbors.insert(Point::new(row - 1, col + 1));
+            bn.neighbors.insert(Point(row - 1, col + 1));
         }
         if row > 0 && col > 0 {
-            bn.neighbors.insert(Point::new(row - 1, col - 1));
+            bn.neighbors.insert(Point(row - 1, col - 1));
         }
 
         bn
@@ -62,7 +55,10 @@ impl BoardNode {
 
 /// Represents the board state
 pub struct Board {
+    // Board positions are stored as a graph of neighbors
     spaces: HashMap<Point, BoardNode>,
+    // Walls are stored as points that are current occupied by a wall.
+    wall_spaces: HashSet<Point>,
 }
 
 impl Board {
@@ -71,28 +67,61 @@ impl Board {
         // Initialize the board
         let mut board = Board {
             spaces: HashMap::new(),
+            wall_spaces: HashSet::new(),
         };
         for r in 0..BOARD_SIZE {
             for c in 0..BOARD_SIZE {
-                board
-                    .spaces
-                    .insert(Point::new(r, c), BoardNode::new(r, c, None));
+                board.spaces.insert(Point(r, c), BoardNode::new(r, c, None));
             }
         }
 
         // Place player colors
         // TODO 4 player?
-        if let Some(bn) = board.spaces.get_mut(&Point::new(0, 2)) {
+        if let Some(bn) = board.spaces.get_mut(&Point(0, 2)) {
             (*bn).contents = Some(Color::Red);
         }
-        if let Some(bn) = board.spaces.get_mut(&Point::new(4, 2)) {
+        if let Some(bn) = board.spaces.get_mut(&Point(4, 2)) {
             (*bn).contents = Some(Color::Blue);
         }
 
         board
     }
+
+    /// Moves a pawn in a specified direction
+    pub fn move_pawn(&mut self, color: Color, direction: Direction) {}
+
+    /// Indicates if a provided wall position is valid
+    pub fn can_place_wall(self, wall: Wall) -> bool {
+        // TODO complete
+        // Check all wall positions, if at least one coordinate matches, a
+        // wall already exists in that position.
+        false
+    }
+
+    /// Places a wall in a specific location
+    pub fn place_wall(&mut self, player: &mut Player, wall: Wall) {
+        // TODO complete
+        // TODO remove neighbors from affected graph nodes.
+    }
 }
 
+/*
+Board Display Diagram
+---------------------
+
+    0    1    2    3    4
+
+0  [_]  [_]  [R]  [_]  [_]  Player Turn: TODO
+                            Walls remaining: 00
+1  [_]||[_]  [_]  [_]  [_]
+      ||
+2  [_]||[_]  [_]  [_]  [_]
+        ========
+3  [_]  [_]  [_]  [_]  [_]
+
+4  [_]  [_]  [B]  [_]  [_]
+
+*/
 impl fmt::Display for Board {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         for r in 0..BOARD_SIZE {
@@ -100,7 +129,7 @@ impl fmt::Display for Board {
                 // TODO print walls
 
                 // Determine player character to render on the board
-                let pos_ch = match self.spaces.get(&Point::new(r, c)) {
+                let pos_ch = match self.spaces.get(&Point(r, c)) {
                     Some(bn) => match bn.contents {
                         Some(Color::Red) => "R",
                         Some(Color::Blue) => "B",
--- src/player.rs
+++ src/player.rs
@@ -3,6 +3,9 @@
 //! Description:    Player representation
 //!
 
+pub const WALL_COUNT_2_PLAYERS: usize = 10;
+pub const WALL_COUNT_4_PLAYERS: usize = 5;
+
 /// Player pawn color
 pub enum Color {
     Red,
@@ -11,15 +14,34 @@ pub enum Color {
     Yellow,
 }
 
+/// Player pawn direction
+pub enum Direction {
+    N,
+    E,
+    S,
+    W,
+    // Only applicable in special passing edge cases.
+    NE,
+    NW,
+    SE,
+    SW,
+}
+
 /// Represents a player
 pub struct Player {
     id: Color,
     wins: usize,
+    wall_cntr: usize,
 }
 
 impl Player {
     pub fn new(id: Color) -> Self {
-        let player = Player { id: id, wins: 0 };
+        let player = Player {
+            id: id,
+            wins: 0,
+            // TODO: 4 player support?
+            wall_cntr: WALL_COUNT_2_PLAYERS,
+        };
         player
     }
 }
